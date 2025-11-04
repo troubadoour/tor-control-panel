672578454a0407dcbb4c04d991fa93e2812993a3
troubadoour <trobador@riseup.net>
2025-11-04 20:07:30 +0000

    import tor_bootstrap

--- usr/lib/python3/dist-packages/tor_control_panel/anon_connection_wizard.py
+++ usr/lib/python3/dist-packages/tor_control_panel/anon_connection_wizard.py
@@ -26,7 +26,7 @@ import fcntl
 from guimessages.translations import _translations
 from guimessages.guimessage import gui_message
 
-from . import tor_status, repair_torrc
+from . import tor_status, repair_torrc, tor_bootstrap
 from .tor_status import cat, write_to_temp_then_move
 from .edit_etc_resolv_conf import edit_etc_resolv_conf_add, edit_etc_resolv_conf_remove
 
@@ -1297,7 +1297,7 @@ class AnonConnectionWizard(QtWidgets.QWizard):
                     after they clicked the NextButton.
                     '''
 
-                    self.bootstrap_thread = TorBootstrap(self)
+                    self.bootstrap_thread = tor_bootstrap.TorBootstrap(self)
                     self.bootstrap_thread.signal.connect(self.update_bootstrap)
                     self.bootstrap_thread.start()
                 elif self.tor_status == 'cannot_connect':
@@ -1544,141 +1544,6 @@ class AnonConnectionWizard(QtWidgets.QWizard):
         elif Common.bridge_type == 'meek-azure':
             Common.bridge_type_with_comment = 'meek-azure'
 
-class TorBootstrap(QtCore.QThread):
-    '''signal will receive the emit from TorBootstrap with two values:
-    bootstrap_phase and bootstrap_percent.
-    It will pass them to the update_bootstrap()
-    '''
-    signal = QtCore.pyqtSignal(str, int)
-
-    def __init__(self, main):
-        #super(TorBootstrap, self).__init__(main)
-        QtCore.QThread.__init__(self, parent=None)
-        self.previous_status = ''
-        bootstrap_percent = 0
-        #self.is_running = False
-        '''The TAG to phase mapping is mainly according to:
-        https://gitweb.torproject.org/tor-launcher.git/tree/src/chrome/locale/en/torlauncher.properties
-        '''
-        self.tag_phase = {'starting': 'Starting',
-                    'conn': 'Connecting to a relay',
-                    'conn_dir': 'Connecting to a relay directory',
-                    'conn_done_pt': "Connected to pluggable transport",
-                    'handshake_dir': 'Finishing handshake with directory server',
-                    'onehop_create': 'Establishing an encrypted directory connection',
-                    'requesting_status': 'Retrieving network status',
-                    'loading_status': 'Loading network status',
-                    'loading_keys': 'Loading authority certificates',
-                    'enough_dirinfo': 'Loaded enough directory info to build circuits',
-                    'ap_conn': 'Connecting to a relay to build circuits',
-                    'ap_conn_done': 'Connected to a relay to build circuits',
-                    'ap_conn_done_pt': 'Connected to pluggable transport to build circuits',
-                    'ap_handshake': 'Finishing handshake with a relay to build circuits',
-                    'ap_handshake_done': 'Handshake finished with a relay to build circuits',
-                    'requesting_descriptors': 'Requesting relay information',
-                    'loading_descriptors': 'Loading relay information',
-                    'conn_or': 'Connecting to the Tor network',
-                    'conn_done': "Connected to a relay",
-                    'handshake': "Handshaking with a relay",
-                    'handshake_or': 'Finishing handshake with first hop',
-                    'circuit_create': 'Establishing a Tor circuit',
-                    'done': 'Connected to the Tor network!'}
-
-
-    def connect_to_control_port(self):
-        import stem
-        import stem.control
-        import stem.socket
-        from stem.connection import connect
-
-        '''Step 1: Construct a Tor controller'''
-        # In case something wrong happened when trying to start Tor,
-        # causing /run/tor/control never be generated.
-        # We set up a time counter and hardcode the wait time limitation as 10s.
-
-        bootstrap_phase = 'Constructing Tor Controller...'
-        bootstrap_percent = 0
-        self.signal.emit(bootstrap_phase, bootstrap_percent)
-
-        count=0
-        while not os.path.exists(Common.control_socket_path) and count < 5:
-            count += 0.2
-            time.sleep(0.2)
-
-        try:
-            tor_controller = stem.control.Controller.from_socket_file(Common.control_socket_path)
-        except stem.SocketError:
-            print('Construct Tor Controller Failed: unable to establish a connection')
-            bootstrap_phase = 'no_controller'
-            bootstrap_percent = 0
-            ## After emitting the `no_controller`,
-            ## update_bootstrap() will pop the messagebox and quit
-            self.signal.emit(bootstrap_phase, bootstrap_percent)
-            ## suspend is really useful because we have to wait for our
-            ## emitted signal really reach update_bootstrap()
-            time.sleep(10)
-
-        '''Step 2: Controller Authentication
-        In order to interact with Tor, we have to do the authentication.
-        '''
-        bootstrap_phase = 'Authenticating the Tor Controller...'
-        bootstrap_percent = 0
-        self.signal.emit(bootstrap_phase, bootstrap_percent)
-
-        try:
-            tor_controller.authenticate(Common.control_cookie_path)
-        except stem.connection.IncorrectCookieSize:
-            pass  #if # TODO: the cookie file's size is wrong
-        except stem.connection.UnreadableCookieFile:
-            # TODO: can we let Tor generate a cookie to fix this situation?
-            print('Tor allows for authentication by reading it a cookie file, \
-            but we cannot read that file (probably due to permissions)')
-            bootstrap_phase = 'cookie_authentication_failed'
-            bootstrap_percent = 0
-            self.signal.emit(bootstrap_phase, bootstrap_percent)
-            time.sleep(10)
-        except stem.connection.CookieAuthRejected:
-            pass  #if cookie authentication is attempted but the socket doesn't accept it
-        except stem.connection.IncorrectCookieValue:
-            pass  #if the cookie file's value is rejected
-
-        return tor_controller
-
-    def run(self):
-        self.tor_controller = self.connect_to_control_port()
-        '''if DisableNetwork is 1, then toggle it to 0
-        because we really want Tor connect to the network'''
-        if self.tor_controller.get_conf('DisableNetwork') == '1':
-            ## TODO: This can be confusing. Config should now have 'DisableNetwork 0',
-            ## Tor already being reload or restarted. If it is still 'DisableNetwork 1',
-            ## then this should be replaced with an error page. This is because Tor would
-            ## refuse to connect after restart without ACW.
-            self.tor_controller.set_conf('DisableNetwork', '0')
-            print('Toggle DisableNetwork value to 0. Tor is now allowed to connect to the network.')
-
-        bootstrap_percent = 0
-        while bootstrap_percent < 100:
-            bootstrap_status = self.tor_controller.get_info("status/bootstrap-phase")
-
-            if bootstrap_status != self.previous_status:
-                bootstrap_percent = int(re.match('.* PROGRESS=([0-9]+).*', bootstrap_status).group(1))
-                bootstrap_tag = re.search(r'TAG=(.*) +SUMMARY', bootstrap_status).group(1)
-                ''' Use TAG= keyword for bootstrap_phase, according to:
-                https://gitweb.torproject.org/tor-launcher.git/plain/README-BOOTSTRAP
-                '''
-                if bootstrap_tag in self.tag_phase:
-                    bootstrap_phase = self.tag_phase[bootstrap_tag]
-                else:
-                    '''Use a static message to cover unknown bootstrap tag to avoid potential
-                    misleading/harmful info shown.'''
-                    bootstrap_phase = "Unknown Bootstrap TAG. This is harmless. Please run this program from command line to view console output and report this."
-                    print('Unknown Bootstrap TAG. Full message is shown in the very next line:')
-                print(bootstrap_status)
-                self.previous_status = bootstrap_status
-                self.signal.emit(bootstrap_phase, bootstrap_percent)
-            time.sleep(0.2)
-        # This will guarantee bootstrap_percent 100 is emitted.
-        self.signal.emit(bootstrap_phase, bootstrap_percent)
 
 
 def main():
